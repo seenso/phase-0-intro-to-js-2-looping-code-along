4144a3dece94b1ffd3d34b191eb9716f02f27fdf
seenso <>
2021-10-04 15:53:38 -0800

    Passed test for writeCards() function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function writeCards(nameArr, giftType) {
+    let cards = [];
+    for (let i = 0; i < nameArr.length; i++) {
+        let message = `Thank you, ${nameArr[i]}, for the wonderful ${giftType} gift!`;
+        cards.push(message);
+    }
+    return cards;
+}
+
+function countDown(number) {}
